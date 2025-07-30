7ffc6f79ca8e1813a0d511a472f4217f6ca2b9a0
Lucas <martelli.lucas14@gmail.com>
2025-07-30 19:52:04 -0300

    Fix: remove unexpected indent on app.run

--- app.py
+++ app.py
@@ -185,4 +185,4 @@ if __name__ == '__main__':
     scheduler.start()
     
     port = int(os.environ.get("PORT", 5000))
-       app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)
